9359d6fc5f32e7e74553cc57b6fc66a28faa9e5c
Behnoosh <behnoosh.moshtagh@gmail.com>
2023-06-24 22:39:03 +0330

    Implement Csv File Writer

--- src/Infrastructure/FileIO/CsvFileReader.php
+++ src/Infrastructure/FileIO/CsvFileReader.php
@@ -2,7 +2,23 @@
 
 namespace App\Infrastructure\FileIO;
 
-class CsvFileReader
-{
+use App\Infrastructure\Contracts\FileReaderInterface;
 
+class CsvFileReader implements FileReaderInterface
+{
+    public  static function readFile(string $filename): ?array
+    {
+        if (!file_exists($filename)) {
+            return null;
+        }
+
+        $rows = [];
+        if (($handle = fopen($filename, "r")) !== false) {
+            while (($data = fgetcsv($handle, 0, ";")) !== false) {
+                $rows[] = $data;
+            }
+            fclose($handle);
+        }
+        return $rows;
+    }
 }
---
+++ src/Infrastructure/FileIO/CsvFileWriter.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Infrastructure\FileIO;
+
+
+use App\Infrastructure\Contracts\FileWriterInterface;
+
+class CsvFileWriter implements FileWriterInterface
+{
+    public function writeFile(string $filename, array $data): void
+    {
+        $handle = fopen($filename, 'w');
+        if ($handle !== false) {
+            foreach ($data as $row) {
+                fputcsv($handle, $row);
+            }
+            fclose($handle);
+        }
+    }
+}
