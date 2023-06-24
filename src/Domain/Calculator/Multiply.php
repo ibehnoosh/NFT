9d84dea7f07f96dbb59b46ef9a54781640300e0e
Behnoosh <behnoosh.moshtagh@gmail.com>
2023-06-24 21:58:21 +0330

    Implement Multiply Calculator

---
+++ src/Domain/Calculator/Multiply.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Calculator;
+
+use App\Domain\Contracts\CalculatorInterface;
+
+class Multiply implements CalculatorInterface
+{
+    public static function calculate(int $num1, int $num2): ?int
+    {
+        return $num1 * $num2;
+    }
+}
