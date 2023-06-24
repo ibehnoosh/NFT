02e33b26de3b411fe3333b82abc30ee8070c28b7
Behnoosh <behnoosh.moshtagh@gmail.com>
2023-06-24 22:17:38 +0330

    implement Command Class

---
+++ src/Application/CSVCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Application;
+
+use App\Application\Contracts\Command;
+
+class CSVCommand implements Command
+{
+
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+        // get command options
+        // read CSV file
+        // calculate
+        // write result and log
+    }
+}
---
+++ src/Application/Contracts/Command.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Application\Contracts;
+
+Interface Command
+{
+    public function execute();
+}
