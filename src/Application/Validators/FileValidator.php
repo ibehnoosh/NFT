6d1f3d25cdc403657ce7423ad6b2d3fba29cd30c
Behnoosh <behnoosh.moshtagh@gmail.com>
2023-06-25 12:04:53 +0330

    Implement File Validator

--- src/Application/Validators/FileValidator.php
+++ src/Application/Validators/FileValidator.php
@@ -6,9 +6,33 @@ use App\Application\Contracts\ValidatorInterface;
 
 class FileValidator implements ValidatorInterface
 {
-    public function validate(array $data): bool
+    private int $maxFileSize = 1048576; // 1 MB
 
+    public function validate(array $data): bool
     {
+        if (!isset($data['file'])) {
+            return false;
+        }
+
+        $file = $data['file'];
+
+        if (!file_exists($file)) {
+            return false;
+        }
+
+        if (!is_readable($file)) {
+            return false;
+        }
+
+        if (filesize($file) > $this->maxFileSize) {
+            return false;
+        }
+
+        $fileInfo = pathinfo($file);
+        if (!isset($fileInfo['extension']) || $fileInfo['extension'] !== 'csv') {
+            return false;
+        }
+
         return true;
     }
 }
